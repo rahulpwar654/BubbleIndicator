9b5d9e43b5ad4b5aba86d4961c8b57a50a8dbdac
rahulpwar654 <rahulpwar654@gmail.com>
2017-06-19 23:20:57 +0530

    set buble

--- app/src/main/java/com/rahulpwar654/bubbleindicator/MainActivity.java
+++ app/src/main/java/com/rahulpwar654/bubbleindicator/MainActivity.java
@@ -13,33 +13,22 @@ public class MainActivity extends AppCompatActivity implements IndicatorClickLis
 
     NumberedIndicator numberedIndicator;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NumberedIndicator numberedIndicator;
         numberedIndicator= (NumberedIndicator) findViewById(R.id.numberedindicator);
-
-        /**
-         *
-         */
-
         numberedIndicator.setBubbleNumbers(4);
+        numberedIndicator.setSelectedBubleNum(0);
         numberedIndicator.setBackgroundColor(Color.WHITE);
         numberedIndicator.setBubbleWidgetClickListener(this);
-
-
-
-
     }
 
     @Override
     public void onBubbleClicked(int bubbleNum) {
-
         Toast.makeText(this, ""+bubbleNum+" clicked ", Toast.LENGTH_SHORT).show();
-
-
+        numberedIndicator.setSelectedBubleNum(0);
     }
 }
